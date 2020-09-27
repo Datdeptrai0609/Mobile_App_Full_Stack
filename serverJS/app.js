2340b04e8bf6b554002570bf250c992bd3237bb6
Datdeptrai0609 <Datdeptrai0609>
2020-09-27 11:27:19 +0700

    add all file

--- serverJS/app.js
+++ serverJS/app.js
@@ -1,24 +1,36 @@
 // define express
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 //define socket io
-const server = require ('http').createServer();
+const server = require ("http").createServer(app);
 const io = require ('socket.io')(server);
 
 
 var path = require('path');
-
+//use JSON to send req res
+app.use(express.json());
+// urlenconded
+app.use(express.urlencoded({ extended: false }));
 //use for static file like css html...
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-
+//connect to monopoly web file 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'public/web.html'));
     console.log(`client join to localhost: ${port}`);
 });
 
-app.listen(port , () => {
-    console.log(`server listening at localhost ${port}`);
+//socketIO-server
+io.on("connection", (socket) => {
+    // console.log(`id connect: ${socket.id}`);
+    console.log(socket.id);
+    socket.on("hello", data => {
+        console.log(`hello ${data}`);
+        server.emit("hello", "done to resend file");
+    })
+});
+server.listen(port, () => {
+    console.log(`server listening at localhost: ${port}`);
 });
